c49b9c469c02288a375bbe645e3558551d0dc1fc
JunayedAdnan <mdjunayedadnan999@gmail.com>
2025-07-16 09:05:17 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var options = {
+        strings: ["Web Developer", "Frontend Developer", "UI/UX Designer", "Freelancer"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true
+    };
+
+    var typed = new Typed(".text", options);
+});
